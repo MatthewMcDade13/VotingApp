e7f2ad84c5d6b097877af3d3df105d3be28ffe0f
Matthew McDade <Matthew.McDade13@gmail.com>
2017-04-28 16:34:54 -0700

    Set up nuget dependencies and added basic routing

--- VotingApp/Startup.cs
+++ VotingApp/Startup.cs
@@ -7,15 +7,39 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json.Serialization;
+using Microsoft.Extensions.Configuration;
 
 namespace VotingApp
 {
     public class Startup
     {
+        public IConfigurationRoot Configuration { get; }
+
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("config.json")
+                .AddEnvironmentVariables();
+
+            Configuration = builder.Build();
+        }
+
+
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc()
+                .AddJsonOptions(options =>
+               {
+                   options
+                   .SerializerSettings
+                   .ContractResolver =
+                   new CamelCasePropertyNamesContractResolver();
+               });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -27,11 +51,18 @@ namespace VotingApp
             {
                 app.UseDeveloperExceptionPage();
             }
-
-            app.Run(async (context) =>
+            else
             {
-                await context.Response.WriteAsync("Hello World!");
-            });
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+           app.UseMvc(config =>
+           {
+               config.MapRoute(
+                   "Default",
+                   "{controller}/{action}/{id?}",
+                   new { controller = "Home", action = "Index" });
+           });
         }
     }
 }

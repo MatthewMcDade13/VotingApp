5fb61dffe7373f98a3e42a0f6906278587b98e73
Matthew McDade <Matthew.McDade13@gmail.com>
2017-05-06 00:46:21 -0700

    removed require HTTPS to remove conflict with heroku

--- VotingApp/Startup.cs
+++ VotingApp/Startup.cs
@@ -36,7 +36,7 @@ namespace VotingApp
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
+            services.AddMvc()
             .AddJsonOptions(options =>
             {
                 options
